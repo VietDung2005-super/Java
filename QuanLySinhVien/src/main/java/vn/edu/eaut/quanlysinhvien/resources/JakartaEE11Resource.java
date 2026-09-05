2659981ad2da1e522af8f5057bc969da8772b7f6
VietDung2005-super <20231937@eaut.edu.vn>
2026-09-05 10:04:25 +0700

    Add files via upload

---
+++ QuanLySinhVien/src/main/java/vn/edu/eaut/quanlysinhvien/resources/JakartaEE11Resource.java
@@ -0,0 +1,20 @@
+package vn.edu.eaut.quanlysinhvien.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee11")
+public class JakartaEE11Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
